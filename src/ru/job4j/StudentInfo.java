12c67b6c99051660e542f1fa27c13dd431a15915
Home <day-22@yandex.ru>
2021-06-05 23:03:21 +0700

    3.2. Фиксация изменений на github [#243564]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Galushko Aleksey");
+        System.out.println("22.01.1900");
+
+    }
+}
